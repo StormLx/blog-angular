d623735418d0364e0e9c6046fa9aabb78aed27dc
Alexandre Caldato <caldato.alexandre@gmail.com>
2022-07-19 13:58:28 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import articles from '../assets/file/articles.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog-angular';
+  articles = articles;
 }
